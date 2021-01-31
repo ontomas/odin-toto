afc359e9b5c937f0e56318e44dea315475311cff
Tomas Oniščiukas <onisciukas.tomas@gmail.com>
2021-01-31 11:44:27 +0200

    Implement BootstrapV5

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import "./style.css";
+// import "./style.css";
+import "./bootstrap.css";
 import { v4 as uuid } from "uuid";
 
 // default project
-const inbox = {
-  tasks: [
-    {
-      title: "Do the dishes",
-      description: "Desc",
-      dueDate: "DueDate",
-      prio: "prio",
-    },
+const data = {
+  inbox: [
     {
       title: "Code a little",
       description: "Desc",
@@ -23,21 +18,26 @@ const inbox = {
       prio: "prio",
     },
   ],
+  personal: [
+    {
+      title: "Do the dishes",
+      description: "Desc",
+      dueDate: "DueDate",
+      prio: "prio",
+    },
+  ],
 };
 
 const DOM = (() => {
   const tasksContainer = document.querySelector(".js-tasks");
-  const addTask = document.querySelector(".js-add-task");
-  const modal = document.querySelector(".js-modal");
-  const modalClose = document.querySelector(".js-modal-close");
+  const addTask = document.getElementById("add-task");
+  const projectForm = document.getElementById("add-project");
   const taskForm = document.getElementById("add-task");
-
-  const constructTask = (task) => {
-    const taskElement = document.createElement("p");
-    taskElement.classList.add("mb-2");
-    taskElement.textContent = task.title;
-    tasksContainer.appendChild(taskElement);
-  };
+  const projectsContainer = document.getElementById("projects");
+  const addModal = new bootstrap.Modal(
+    document.getElementById("add-task-modal")
+  );
+  const taskProject = document.getElementById("task-project");
 
   const renderTasks = (tasks) => {
     tasksContainer.innerHTML = "";
@@ -46,15 +46,71 @@ const DOM = (() => {
     });
   };
 
-  const showModal = () => {
-    modal.classList.remove("hidden");
+  const constructTask = (task) => {
+    const taskWrapper = document.createElement("div");
+    taskWrapper.classList.add("form-check");
+    const input = document.createElement("input");
+    input.classList.add("form-check-input");
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("id", task.id);
+    const label = document.createElement("label");
+    label.classList.add("form-check-label");
+    label.setAttribute("for", task.id);
+    label.textContent = task.title;
+    taskWrapper.appendChild(input);
+    taskWrapper.appendChild(label);
+    tasksContainer.appendChild(taskWrapper);
   };
 
-  const hideModal = () => {
-    modal.classList.add("hidden");
+  const renderProjects = (projects) => {
+    projectsContainer.innerHTML = "";
+    projects.map((project, index) => {
+      constructProject(project, index);
+    });
   };
 
-  return { renderTasks, addTask, modalClose, showModal, hideModal, taskForm };
+  const constructProject = (project, index) => {
+    const projectElement = document.createElement("li");
+    projectElement.classList.add("nav-item");
+    const link = document.createElement("a");
+    link.classList.add("nav-link", `${index === 0 && "active"}`);
+    link.style.textTransform = "capitalize";
+    link.setAttribute("href", "#");
+    link.textContent = project;
+    projectElement.appendChild(link);
+    projectsContainer.appendChild(projectElement);
+  };
+
+  return {
+    renderTasks,
+    renderProjects,
+    addTask,
+    taskForm,
+    projectForm,
+    addModal,
+    projectsContainer,
+    taskProject,
+  };
+})();
+
+const projects = (() => {
+  const createProject = (title) => {
+    if (!Object.keys(data).indexOf(title.toLowerCase()) > -1) {
+      data[title.toLowerCase()] = [];
+    }
+    DOM.projectForm.reset();
+    DOM.addModal.hide();
+  };
+  const updateProjectSelection = () => {
+    DOM.taskProject.innerHTML = "";
+    Object.keys(data).map((project) => {
+      const option = document.createElement("option");
+      option.setAttribute("value", project);
+      option.textContent = project;
+      DOM.taskProject.appendChild(option);
+    });
+  };
+  return { createProject, updateProjectSelection };
 })();
 
 const tasks = (() => {
@@ -63,24 +119,43 @@ const tasks = (() => {
     return { id, title, description, dueDate, prio };
   };
 
-  const createTask = (title, description, dueDate, prio) => {
+  const createTask = (project, title, description, dueDate, prio) => {
     const newTask = task(title, description, dueDate, prio);
-    inbox.tasks.push(newTask);
+    data[project].push(newTask);
+    DOM.taskForm.reset();
+    DOM.addModal.hide();
   };
   return { createTask };
 })();
 
-DOM.renderTasks(inbox.tasks);
-DOM.addTask.addEventListener("click", DOM.showModal);
-DOM.modalClose.addEventListener("click", DOM.hideModal);
+DOM.renderTasks(data.inbox);
+
+// Submit new task
 DOM.taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   tasks.createTask(
-    document.getElementById("title").value,
-    document.getElementById("description").value,
-    document.getElementById("date").value,
-    document.getElementById("priority").value
+    document.getElementById("task-project").value,
+    document.getElementById("task-title").value,
+    document.getElementById("task-description").value,
+    document.getElementById("task-date").value,
+    document.getElementById("task-priority").value
   );
-  DOM.renderTasks(inbox.tasks);
-  DOM.hideModal();
+  DOM.renderTasks(data.inbox);
 });
+
+// Submit new project
+DOM.projectForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.getElementById("project-title").value;
+  projects.createProject(title);
+  DOM.renderProjects(Object.keys(data));
+  projects.updateProjectSelection();
+});
+
+// render project in the sidebar
+DOM.renderProjects(Object.keys(data));
+// add projects
+
+// add an ability to select where new tasks goes - which project
+// render all project in the task form
+window.addEventListener("DOMContentLoaded", projects.updateProjectSelection);
